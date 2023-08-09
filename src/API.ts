438e80345ba1d9581389e517fee95a1a50e3e6b0
RCD Hui <rcdhui@gmail.com>
2023-08-09 09:29:59 -0400

    add config

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
+const host = process.env.HOST || 'https://jsonplaceholder.typicode.com'
 class API {
     constructor() {
         console.log("API constructor")
     }
 
     async callAPI(id: string){
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/'+ id)
+        const response = await axios.get(host+'/todos/'+ id)
         return response.data
     }
 }
